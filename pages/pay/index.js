2aca901d47d62bc0b1fc6a5434fa61da7254a886
= <=>
2019-09-11 23:16:41 +0800

    update:pay

---
+++ pages/pay/index.js
@@ -0,0 +1,77 @@
+// pages/pay/index.js
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    wxPay: true
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+
+  },
+
+
+  choosePayWay: function (e) {
+    let {type} = e.currentTarget.dataset
+    console.log(type)
+    type === 'wx' ? this.setData({wxPay: true}) : this.setData({wxPay: false});
+    
+  },
+  submitPay: function () {
+
+    console.log('是否为微信自己支付: ' + this.data.wxPay)
+  },
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+
+  }
+})
